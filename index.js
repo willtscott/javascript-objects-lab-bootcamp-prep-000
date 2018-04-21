2cd58206eb2df776e241ee5cfd96b20e0963b305
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 01:42:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+ var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ return Object.assign(object, {[key]: value})}
+function deleteFromObjectByKey(object, key){
+  var newObj = {}
+  Object.assign(newObj, object)
+  delete newObj[key]
+  return newObj}
